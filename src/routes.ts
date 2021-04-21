32ebd3243ff97839db8748bd6ef6f86adb5f4737
Carlos-Silva <carlosgsilva.dev@gmail.com>
2021-04-20 21:39:32 -0400

    Criando as rotas de configuração

--- src/routes.ts
+++ src/routes.ts
@@ -1,23 +1,13 @@
 import { request, response, Router } from "express";
 import { getCustomRepository } from "typeorm";
+import { SettingsController } from "./controllers/SettingsController";
 
 import { SettingsRepository } from "./repositories/SettingsRepository"
 
 const routes = Router();
 
-routes.post("/settings", async (request, response) => {
-    const { chat, username } = request.body;
+const settingsController = new SettingsController();
 
-    const settingsRepository = getCustomRepository(SettingsRepository);
-
-    const settings = settingsRepository.create({
-        chat,
-        username
-    })
-
-    await settingsRepository.save(settings);
-
-    return response.json(settings);
-})
+routes.post("/settings", settingsController.create);
 
 export { routes };
