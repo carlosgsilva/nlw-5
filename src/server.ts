d99288b42de57502be20c90b154115645f128688
Carlos-Silva <carlosgsilva.dev@gmail.com>
2021-04-20 21:23:15 -0400

    Criando os repositórios e as rotas de configuração

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,11 @@
 import express, { response } from "express";
 
 import './database';
+import { routes } from "./routes";
 
 const app = express();
 
-/**
- * GET = Buscar
- * POST = Criação
- * PUT = Alteração
- * DELETE = Deletar
- * PATCH = Alterar uma informação específica
- */
+app.use(express.json());
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Olá nlw 05!' });
-});
-
-app.post('/', (request, response) => {
-    return response.json({ message: 'Cadastro realizado com sucesso!' })
-})
-
+app.use(routes);
 app.listen(3333, () => console.log("Server is running on port 3333"));
